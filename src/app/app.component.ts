4f89afdd68b8dd97a9553a069b164c94069a707f
WessamSaeid <wessam.saeid33@gmail.com>
2018-06-27 14:13:36 +0200

    removing unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgForm, NgModel } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 import { ApiService } from './api.service';
 
